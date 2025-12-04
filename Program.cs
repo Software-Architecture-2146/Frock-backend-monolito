092e9c7fc009db3e6dcc2cc848e10f8b53bc4870
Mitawarmi2000 <u202214130@upc.edu.pe>
2025-12-03 19:24:38 -0500

    feat: update

--- Program.cs
+++ Program.cs
@@ -211,7 +211,7 @@ builder.Services.AddCors(options =>
     {
         policy.WithOrigins(
                 "http://localhost:5173",
-                "https://frock-frontend.vercel.app/",
+                "https://frock-frontend.vercel.app",
                 "https://frock-backend-monolito.onrender.com" // ⬅️ AGREGAR ESTO (ajustaremos la URL exacta después)
                 
             )
